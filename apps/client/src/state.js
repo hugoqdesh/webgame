a2213071e44e4138d8cb2e2c666f76b6035c2626
Lauri Musto <laurimusto@gmail.com>
2026-05-22 19:13:38 +0300

    added world and player state

--- apps/client/src/state.js
+++ apps/client/src/state.js
@@ -1,5 +1,44 @@
 export const state = {
-  playerId: null,
-  snapshots: [],
-  latest: null
+  world: {
+    width: 800,
+    height: 600,
+  },
+
+  players: {
+    player1: {
+      id: "player1",
+      health: 100,
+      posx: 100,
+      posy: 100,
+      vx: 0,
+      vy: 0,
+    },
+
+    player2: {
+      id: "player2",
+      health: 100,
+      posx: 100,
+      posy: 100,
+      vx: 0,
+      vy: 0,
+    },
+
+    player3: {
+      id: "player3",
+      health: 100,
+      posx: 300,
+      posy: 300,
+      vx: 0,
+      vy: 0,
+    },
+
+    player4: {
+      id: "player4",
+      health: 100,
+      posx: 400,
+      posy: 400,
+      vx: 0,
+      vy: 0,
+    },
+  },
 };
