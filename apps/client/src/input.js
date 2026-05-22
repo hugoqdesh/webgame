6e397899374e91e991efc3566cc21fad31e78d8b
Lauri Musto <laurimusto@gmail.com>
2026-05-22 20:53:56 +0300

    player movement input detection

--- apps/client/src/input.js
+++ apps/client/src/input.js
@@ -1,3 +1,40 @@
+import { state } from "./state.js";
+
 export function initInput() {
   // TODO: capture keyboard input and send to server
+  addEventListener("keydown", (event) => {
+    const myPlayer = state.players.player1;
+    switch (event.key) {
+      case "ArrowRight":
+        myPlayer.vx = 2;
+        break;
+      case "ArrowLeft":
+        myPlayer.vx = -2;
+        break;
+      case "ArrowUp":
+        myPlayer.vy = 2;
+        break;
+      case "ArrowDown":
+        myPlayer.vy = -2;
+        break;
+    }
+  });
+
+  addEventListener("keyup", (event) => {
+    const myPlayer = state.players.player1;
+    switch (event.key) {
+      case "ArrowRight":
+        myPlayer.vx = 0;
+        break;
+      case "ArrowLeft":
+        myPlayer.vx = 0;
+        break;
+      case "ArrowUp":
+        myPlayer.vy = 0;
+        break;
+      case "ArrowDown":
+        myPlayer.vy = 0;
+        break;
+    }
+  });
 }
