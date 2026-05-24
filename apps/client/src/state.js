1647131d6cb080eb829085de768d7356024f7601
Lauri Musto <laurimusto@gmail.com>
2026-05-24 17:38:38 +0300

    client side state added for local state

--- apps/client/src/state.js
+++ apps/client/src/state.js
@@ -1,44 +1,5 @@
-export const state = {
-  world: {
-    width: 900,
-    height: 600,
-  },
-
-  players: {
-    player1: {
-      id: "player1",
-      health: 100,
-      x: 100,
-      y: 100,
-      vx: 0,
-      vy: 0,
-    },
-
-    player2: {
-      id: "player2",
-      health: 100,
-      x: 100,
-      y: 100,
-      vx: 0,
-      vy: 0,
-    },
-
-    player3: {
-      id: "player3",
-      health: 100,
-      x: 300,
-      y: 300,
-      vx: 0,
-      vy: 0,
-    },
-
-    player4: {
-      id: "player4",
-      health: 100,
-      x: 400,
-      y: 400,
-      vx: 0,
-      vy: 0,
-    },
-  },
+// This is the client's local copy of the game world.
+// It starts empty. It will be updated whenever the server sends a network message.
+export const clientState = {
+    players: {}
 };
